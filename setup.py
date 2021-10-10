3cb81b14b21d66198ac851b8128ee446781037c6
juenhint <73236765+juenhint@users.noreply.github.com>
2021-10-10 11:24:04 +0300

    First template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='jhfuncs',
+version='2.0',
+description='Tools for dataframe handling and analysis',
+url='#',
+author='Jukka Hintikka',
+author_email='juenhint@jyu.fi',
+license='MIT',
+packages=['jhfuncs'],
+zip_safe=False)
